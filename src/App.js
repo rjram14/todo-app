35da35a95b8a7c188e090a7b6338d8f1077397f9
rjram14 <ramnewasgkp@gmail.com>
2021-06-14 16:12:19 +0530

    todo-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TODOAPP from "../src/components/ToDOList/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <TODOAPP />
+    </div>
+  );
+}
+
+export default App;
